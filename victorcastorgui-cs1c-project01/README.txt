c169228daef83f9823487b0686c947a188d256bb
Victor Castor <55765661+victorcastorgui@users.noreply.github.com>
2021-11-27 13:08:24 -0500

    Add files via upload

---
+++ victorcastorgui-cs1c-project01/README.txt
@@ -0,0 +1,13 @@
+project folder:
+victorcastorgui-cs1c-project01/
+
+Brief description of submitted files:
+
+src/DemoGit.java
+    - println method to print out my name.
+
+resources/RUN.txt
+    - RUN
+
+README.txt
+    - description of submitted files
---
+++ victorcastorgui-cs1c-project01/src/DemoGit.java
@@ -0,0 +1,24 @@
+/**
+ * Simple class to test submitting to GitHub repository.
+ * @author Foothill College, Bita M, Victor Castor
+ *
+ * Note: If you modified this file then update the author name with your: FirstName LastName
+ *
+ * This is a simple program made to check if I am able to commit to the repository in GitHub.
+ * This program prints out my full name.
+ * Reminder: Take a look at "Example Program Submission" in course Modules and follow the
+ *           "Program Guidelines" to make sure your submission is complete.
+ */
+public class DemoGit 
+{
+
+	/**
+	 * Outputs my name.
+	 * @param args  Not used.
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello. My name is Victor Castor");
+	}
+
+}
